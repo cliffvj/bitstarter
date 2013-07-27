c9373bc47450d3c65fdda7409b408e45cc8861fb
Ubuntu <ubuntu@ip-172-31-4-218.ap-southeast-1.compute.internal>
2013-07-27 14:59:04 +0000

    Added the new index.html and renamed old index.html to index_old.html

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var express = require('express');
-
 var fs = require('fs');
+var htmlfile = "index.html";
 
-var indexhello = fs.readFileSync('index.html');
+//var indexhello = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-        response.send(indexhello.toString());
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
